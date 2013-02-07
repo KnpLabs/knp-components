bc40ed03f28b3f9d4f1cd160013fe4c9a530dd70
Joseph Bielawski <stloyd@gmail.com>
2013-02-07 09:42:22 +0100

    Fix CS from previous merge

--- src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/DBALQueryBuilderSubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Paginate/Doctrine/DBALQueryBuilderSubscriber.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Knp\Component\Pager\Event\Subscriber\Paginate\Doctrine;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -12,42 +13,51 @@ use Doctrine\DBAL\Query\QueryBuilder;
  */
 class DBALQueryBuilderSubscriber implements EventSubscriberInterface
 {
-	public function items(ItemsEvent $event)
-	{
-		if ($event->target instanceof QueryBuilder) {
-			/** @var QueryBuilder $target */
-			$target = $event->target;
+    public function items(ItemsEvent $event)
+    {
+        if ($event->target instanceof QueryBuilder) {
+            /** @var $target QueryBuilder */
+            $target = $event->target;
+        
+            // get the query
+            $sql = $target->getSQL();
 
-			//get the query
-			$sql = $target->getSQL();
+            // count results
+            $qb = clone $target;
+            $qb
+                ->resetQueryParts()
+                ->select('count(*) as cnt')
+                ->from('(' . $sql . ')', 'ololoshke_trololoshke')
+            ;
 
-			//count results
-			$qb = clone $target;
-			$qb->resetQueryParts()
-				->select('count(*) as cnt')
-				->from('(' . $sql . ')', 'ololoshke_trololoshke');
-			$event->count = $qb->execute()
-				->fetchColumn(0);
+            $event->count = $qb
+                ->execute()
+                ->fetchColumn(0)
+            ;
 
-			//if there is results
-			$event->items = array();
-			if ($event->count) {
-				$q = clone $target;
-				$q->setFirstResult($event->getOffset())
-					->setMaxResults($event->getLimit());
+            // if there is results
+            $event->items = array();
+            if ($event->count) {
+                $qb = clone $target;
+                $qb
+                    ->setFirstResult($event->getOffset())
+                    ->setMaxResults($event->getLimit())
+                ;
+                
+                $event->items = $qb
+                    ->execute()
+                    ->fetchAll()
+                ;
+            }
+            
+            $event->stopPropagation();
+        }
+    }
 
-				$event->items = $q->execute()
-					->fetchAll();
-			}
-
-			$event->stopPropagation();
-		}
-	}
-
-	public static function getSubscribedEvents()
-	{
-		return array(
-			'knp_pager.items' => array('items', 10 /*make sure to transform before any further modifications*/)
-		);
-	}
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'knp_pager.items' => array('items', 10 /*make sure to transform before any further modifications*/)
+        );
+    }
 }
