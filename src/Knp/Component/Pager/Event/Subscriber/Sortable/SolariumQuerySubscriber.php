f15be1da83bfa545c788358992a8d541a8b234b7
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2013-09-10 10:53:58 +0200

    Fixed bug introduced in pull request #82

--- src/Knp/Component/Pager/Event/Subscriber/Sortable/SolariumQuerySubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Sortable/SolariumQuerySubscriber.php
@@ -15,7 +15,9 @@ class SolariumQuerySubscriber implements EventSubscriberInterface
     public function items(ItemsEvent $event)
     {
         if (is_array($event->target) && 2 == count($event->target)) {
-            list($client, $query) = $event->target;
+            $values = array_values($event->target);
+            list($client, $query) = $values;
+
             if ($client instanceof \Solarium\Client && $query instanceof \Solarium\QueryType\Select\Query\Query) {
                 if (isset($_GET[$event->options['sortFieldParameterName']])) {
                     if (isset($event->options['sortFieldWhitelist'])) {
