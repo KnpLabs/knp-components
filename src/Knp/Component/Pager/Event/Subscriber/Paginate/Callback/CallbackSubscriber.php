c6beaec3425a598ccbcea1daf2b952ddd2e6a684
Athlan <athlan@vgroup.pl>
2019-07-03 22:25:00 +0200

    Callback pagination

--- src/Knp/Component/Pager/Event/Subscriber/Paginate/Callback/CallbackSubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Paginate/Callback/CallbackSubscriber.php
@@ -6,6 +6,11 @@ namespace Knp\Component\Pager\Event\Subscriber\Paginate\Callback;
 use Knp\Component\Pager\Event\ItemsEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Callback pagination.
+ *
+ * @author Piotr Pelczar <me@athlan.pl>
+ */
 class CallbackSubscriber implements EventSubscriberInterface
 {
     public function items(ItemsEvent $event)
