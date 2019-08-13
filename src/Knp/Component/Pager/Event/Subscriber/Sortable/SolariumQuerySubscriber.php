53eab139e5305e28de5fe77ec7193b17d7a4c798
Fran Moreno <franmomu@gmail.com>
2019-08-13 13:34:50 +0200

    Fix sorting with 2 items

--- src/Knp/Component/Pager/Event/Subscriber/Sortable/SolariumQuerySubscriber.php
+++ src/Knp/Component/Pager/Event/Subscriber/Sortable/SolariumQuerySubscriber.php
@@ -32,13 +32,12 @@ class SolariumQuerySubscriber implements EventSubscriberInterface
             return;
         }
 
-        if (is_array($event->target) && 2 == count($event->target)) {
-            $event->setCustomPaginationParameter('sorted', true);
-
+        if (is_array($event->target) && 2 === count($event->target)) {
             $values = array_values($event->target);
             [$client, $query] = $values;
 
             if ($client instanceof \Solarium\Client && $query instanceof \Solarium\QueryType\Select\Query\Query) {
+                $event->setCustomPaginationParameter('sorted', true);
                 if ($this->request->query->has($event->options[PaginatorInterface::SORT_FIELD_PARAMETER_NAME])) {
                     if (isset($event->options[PaginatorInterface::SORT_FIELD_WHITELIST])) {
                         if (!in_array($this->request->query->get($event->options[PaginatorInterface::SORT_FIELD_PARAMETER_NAME]), $event->options[PaginatorInterface::SORT_FIELD_WHITELIST])) {
