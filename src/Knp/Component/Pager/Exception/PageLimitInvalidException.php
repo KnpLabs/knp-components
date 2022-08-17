615d399fdcb833556db1d12d1adbe2ee183d8566
Karel Souffriau <karel@hardware.info>
2022-08-17 10:19:54 +0200

    Remove properties from exception

--- src/Knp/Component/Pager/Exception/PageLimitInvalidException.php
+++ src/Knp/Component/Pager/Exception/PageLimitInvalidException.php
@@ -6,26 +6,10 @@ use OutOfRangeException;
 
 final class PageLimitInvalidException extends OutOfRangeException
 {
-    private ?int $limit = null;
-
-    public function setLimit(int $limit): void
-    {
-        $this->limit = $limit;
-    }
-
-    public function getLimit(): ?int
-    {
-        return $this->limit;
-    }
-
     public static function create(int $limit): self
     {
-        $exception = new self(
+        return new self(
             sprintf('Invalid page limit. Limit: %d: $limit must be positive non-zero integer', $limit)
         );
-
-        $exception->setLimit($limit);
-
-        return $exception;
     }
 }
--- src/Knp/Component/Pager/Exception/PageNumberInvalidException.php
+++ src/Knp/Component/Pager/Exception/PageNumberInvalidException.php
@@ -6,26 +6,10 @@ use OutOfRangeException;
 
 final class PageNumberInvalidException extends OutOfRangeException
 {
-    private ?int $page = null;
-
-    public function setPage(int $page): void
-    {
-        $this->page = $page;
-    }
-
-    public function getPage(): int
-    {
-        return $this->page;
-    }
-
     public static function create(int $page): self
     {
-        $exception = new self(
+        return new self(
             sprintf('Invalid page number. Page: %d: $page must be positive non-zero integer', $page)
         );
-
-        $exception->setPage($page);
-
-        return $exception;
     }
 }
